c289024cd3b45c5dc86802a584f9bf1cdb8f39c9
shubham51s <shubhmasolat51@gmail.com>
2023-10-22 16:49:51 +0530

    script added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 //Write your code here
-
-
-
-
+class API {
+  #secure;
+  constructor(url) {
+    this.url = url;
+    this.method = "GET";
+    this.#secure = url.startsWith("https");
+  }
+  isSecure() {
+    return this.#secure;
+  }
+  updateUrl(url) {
+    this.#secure = url.startsWith("https");
+  }
+}
 
 //Do not remove this code
-module.exports = { API }
+module.exports = { API };
