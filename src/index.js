cf38df94c737c559e350b431792cb05b0adf0e0f
shubham51s <shubhmasolat51@gmail.com>
2023-10-23 17:52:33 +0530

    changed all

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-//Write your code here
 class API {
   #secure;
-  constructor(url) {
+
+  constructor(url, method = "GET") {
     this.url = url;
-    this.method = "GET";
+
+    this.method = method;
+
     this.#secure = url.startsWith("https");
   }
+
   isSecure() {
     return this.#secure;
   }
+
   updateUrl(url) {
+    this.url = url;
+
     this.#secure = url.startsWith("https");
   }
 }
 
-//Do not remove this code
 module.exports = { API };
